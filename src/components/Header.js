06600e925505ea1238a9aa85d6db5a9207496d89
Daniel <danielduyilemi@gmail.com>
2022-09-07 22:20:39 +0100

    edit header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ const Header = ({ isDesktop, navToggle, navOpen }) => {
 	return (
 		<header
 			className={`flex h-[50vh] bg-header bg-cover font-barlow-condensed font-bold ${
-				isDesktop ? 'text-[5rem]' : ' justify-between px-4 items-center'
+				isDesktop ? 'text-[5rem]' : ' justify-between px-4 items-start p-4'
 			} ${isDesktop && 'pt-10 pl-20'}`}
 		>
-			<h1 className={`${!isDesktop && "text-[2.5rem]"}`}>Weather Blog App</h1>
+			<h1 className={`${!isDesktop && "text-[2rem]"}`}>Weather Blog App</h1>
 			{!isDesktop && <MobileNav navOpen={navOpen} navToggle={navToggle} />}
 		</header>
 	);
