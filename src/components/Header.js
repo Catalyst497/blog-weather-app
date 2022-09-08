9e513bb877826817f2927b52e0e9ba678f110e3b
Daniel <danielduyilemi@gmail.com>
2022-09-08 12:26:27 +0100

    add p-8 to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ isDesktop, navToggle, navOpen }) => {
 	return (
 		<header
 			className={`flex h-[50vh] bg-header bg-cover font-barlow-condensed font-bold ${
-				isDesktop ? 'text-[5rem]' : ' justify-between px-4 items-start pt-4'
+				isDesktop ? 'text-[5rem]' : ' justify-between px-4 items-start pt-8'
 			} ${isDesktop && 'pt-10 pl-20'}`}
 		>
 			<h1 className={`${!isDesktop && "text-[2rem]"}`}>Weather Blog App</h1>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './css/style.css';
 import App from './App';
+
+// ReactDOM.render(
+// 	<Router>
+// 		<App />
+// 	</Router>,
+// 	document.getElementById('root')
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
