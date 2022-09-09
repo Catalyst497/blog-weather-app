21182212bb75d4a8a0eb79afac0ad235fef34fc3
Daniel <danielduyilemi@gmail.com>
2022-09-09 20:37:23 +0100

    Add search bar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,14 @@ const Header = ({ isDesktop, navToggle, navOpen }) => {
 				isDesktop ? 'text-[5rem]' : ' justify-between px-4 items-start pt-8'
 			} ${isDesktop && 'pt-10 pl-20'}`}
 		>
-			<h1 className={`${!isDesktop && "text-[2rem]"}`}>Weather Blog App</h1>
+			<h1 className={`${!isDesktop && 'text-[2rem]'}`}>
+				Weather Blog App{' '}
+				<p className="text-[1rem] font-regular font-barlow max-w-[70%]">
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic,
+					recusandae. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+					A, non!
+				</p>
+			</h1>
 			{!isDesktop && <MobileNav navOpen={navOpen} navToggle={navToggle} />}
 		</header>
 	);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
 import Li from './Li';
-
+import { FaSearch } from 'react-icons/fa';
 export default function Navbar() {
+	FaSearch ? console.log(FaSearch) : console.log(false);
 	return (
-		<nav className="p-2 mx-auto w-[90%] bg-black rounded-full md:rounded-2xl">
-			<ul className="flex justify-between gap-[2rem] max-w-[70%] mx-auto">
+		<nav className="flex p-2 mx-auto w-[90%] bg-black rounded-full md:rounded-2xl">
+			<div className="flex items-center gap-[1rem] bg-white rounded-[0.5rem] border-gray-100">
+				<div className="basis-8 text-center text-gray-500 text-[1rem] px-2 ">
+					<FaSearch />
+				</div>
+				<input
+					type="text"
+					placeholder="Search"
+					className="bg-transparent border-none placeholer:text-gray-500"
+				/>
+			</div>
+			<ul className="flex justify-end gap-[2rem] mx-auto">
 				<Li url="" cont="home" />
 				<Li url="fabrication" cont="fabrication" />
 				<Li url="utilisation" cont="utilisation " />
