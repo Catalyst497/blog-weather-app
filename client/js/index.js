5318c8dce72e9e981eac3cecbaecd0017afea363
Daniel <danielduyilemi@gmail.com>
2022-09-12 21:54:58 +0100

    Do the styles

--- client/js/index.js
+++ client/js/index.js
@@ -1,38 +1,44 @@
+const navLinks = document.querySelectorAll('nav ul a');
+
 // Create route func
 const route = (e) => {
-    e = e || window.event;
-    e.preventDefault();
-    window.history.pushState({}, "", e.target.href);
-    handleLocation();
-}
+	e = e || window.event;
+	e.preventDefault();
+	navLinks.forEach((a) => a.classList.remove('active'));
+	e.target.classList.add('active');
+	window.history.pushState({}, '', e.target.href);
+	handleLocation();
+};
 
 // Give global access to route func
 window.route = route;
 
 // Handling components render
-const blogContainer = document.getElementById("main")
+const blogContainer = document.getElementById('main');
 const routes = {
-    "/": "../public/components/Accueil.html",
-    "/Fabrication": "../public/components/Fabrication.html",
-    "/Utilisation": "../public/components/Utilisation.html",
-    "/Elimination": "../public/components/Elimination.html",
-    "/Solution": "../public/components/Solution.html",
-}
+	'/': '../public/components/Accueil.html',
+	'/Fabrication': '../public/components/Fabrication.html',
+	'/Utilisation': '../public/components/Utilisation.html',
+	'/Elimination': '../public/components/Elimination.html',
+	'/Solution': '../public/components/Solution.html',
+};
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route, {
-			method: 'GET', // *GET, POST, PUT, DELETE, etc.
-			mode: 'cors', // no-cors, *cors, same-origin
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: 'same-origin', // include, *same-origin, omit
-		}).then((data) => data.text());
-    blogContainer.innerHTML = html;
-}
+	const path = window.location.pathname;
+	const route = routes[path] || routes[404];
+	const html = await fetch(route, {
+		method: 'GET', // *GET, POST, PUT, DELETE, etc.
+		mode: 'cors', // no-cors, *cors, same-origin
+		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: 'same-origin', // include, *same-origin, omit
+	}).then((data) => data.text());
+	blogContainer.innerHTML = html;
+};
 
 window.onpopstate = handleLocation;
 handleLocation();
-
+navLinks.forEach((a) => {
+	if (a.getAttribute('href') == location.pathname) a.classList.add('active');
+});
 
 // Handling the Weather API call
 let lon;
@@ -46,6 +52,7 @@ let temperature = document.querySelector('.temperature');
 let sunrise = document.querySelector('.sunrise');
 let sunset = document.querySelector('.sunset');
 let icon = document.querySelector('.weather-icon');
+let datee = document.querySelector('.dt');
 const kelvin = 273;
 
 window.addEventListener('load', () => {
@@ -75,23 +82,18 @@ window.addEventListener('load', () => {
 					weather.textContent = data.weather[0].main;
 					description.textContent = data.weather[0].description;
 					temperature.textContent = Math.floor(data.main.temp - kelvin) + '°C';
-					sunrise.textContent = new Date(
-						data.sys.sunrise * 1000
-					).toLocaleTimeString('en-IN');
-					sunset.textContent = new Date(
-						data.sys.sunset * 1000
-					).toLocaleTimeString('en-IN');
 					let icon1 = data.weather[0].icon;
 					icon.setAttribute(
 						'src',
 						`https://openweathermap.org/img/w/${icon1}.png`
 					);
+					datee.innerHTML = `${moment().format(
+						'dddd'
+					)}, ${moment().format('LL')}`;
 				});
 		});
 	}
 });
-
-
 
 // const btnElRef = document.getElementById("yo");
 
